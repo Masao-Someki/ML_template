db5a949f4c7a8a5ad85dbf662978d457a902d163
masao <Masao-Someki>
2020-03-30 00:42:02 +0900

    Modified path.sh

--- path.sh
+++ path.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda-10.0/lib64
-export CUDA_DEVICE_ORDER=PCI_BUS_ID
+#export CUDA_DEVICE_ORDER=PCI_BUS_ID
 
 export PATH=$PATH:${PWD}/tools/commands
 export PYTHONPATH=$PYTHONPATH:${PWD}/src/net:${PWD}/src/utils:${PWD}/src/writer
